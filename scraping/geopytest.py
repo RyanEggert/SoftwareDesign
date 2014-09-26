9f77a71aedf4f3eaa6533f522013cb0892071b12
Ryan Eggert <RyanEggertWI@gmail.com>
2014-09-26 14:58:35 -0400

    Can extract flight carrier & number for multiple flight legs.

--- scraping/geopytest.py
+++ scraping/geopytest.py
@@ -1,3 +1,17 @@
 ### geopytest.py ###
 
 ## Script for experimenting with geopy. Geopy should be used to convert an input location to a set of coordinates.
+
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+
+while True:
+    inLoc = raw_input('Where are you?\t')
+    location = geolocator.geocode(inLoc)
+    if location==None:
+        print "\nWe're sorry, we cannot find your location. Consider trying again with a more generic location. For example, instead of \"2243 East Loyola Street, MyTown\", try \"Loyola Street, MyTown\".\n"
+    else:
+        break
+   
+print location.address
+print '('+str(location.latitude) + ','+str(location.longitude)+')'
